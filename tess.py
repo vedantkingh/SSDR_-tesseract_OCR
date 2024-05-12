505c097fd416d7f4a8df036526eed093f8c6fddd
vedantkingh <vedantkingh@gmail.com>
2024-05-12 14:58:10 +0530

    Switching to other approach

--- tess.py
+++ tess.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 import pytesseract
 import os
 import matplotlib.pyplot as plt
@@ -7,6 +6,8 @@ import matplotlib.pyplot as plt
 
 def preprocess_image(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    cv2.imwrite(os.path.basename("gray.png"), gray)
 
     hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
 
@@ -19,12 +20,15 @@ def preprocess_image(image):
 
     blur_image = cv2.GaussianBlur(gray, (5, 5), 0)
     thresh = cv2.adaptiveThreshold(blur_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 35, 2)
-    filename = os.path.basename("thresh_seven_segment_display.png")
-    cv2.imwrite(filename, thresh)
+    cv2.imwrite(os.path.basename("thresh.png"), thresh)
     return thresh
 
 def find_contours(thresh):
-    contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    contoured_img = cv2.drawContours(thresh, contours, -1, (0, 255, 0), 2)
+    cv2.imwrite(os.path.basename("contours.png"), contoured_img)
+
     return contours
 
 def filter_contours(contours, min_area=100):
